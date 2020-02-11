af36cb306ebedf8a7fc8acd931fc2eb1fe6af15f
Alexander Steiner <info@alexsteiner.de>
2020-02-11 07:25:56 +0100

    Remove unneeded code from early days

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -11,39 +11,3 @@ public func app(_ env: Environment) throws -> Application {
     try boot(app)
     return app
 }
-
-let userType = try! GraphQLObjectType(name: "User", description: nil, fields: [
-    "firstname": GraphQLField(type: GraphQLNonNull(GraphQLString)),
-    "lastname": GraphQLField(type: GraphQLNonNull(GraphQLString))
-])
-
-struct User: Codable {
-    var firstname: String
-    var lastname: String
-
-}
-
-let schema = try! GraphQLSchema(
-    query: GraphQLObjectType(
-        name: "RootQueryType",
-        fields: [
-            "hello": GraphQLField(
-                type: GraphQLString,
-                resolve: { _, _, _, _ in
-                    "world"
-                }
-            ),
-            "myUser": GraphQLField(type: GraphQLString, resolve: {  _, _, _, _ in
-                "User"
-            }),
-            "user": GraphQLField(type: userType, resolve: { (_, _, _, _) in
-                User(firstname: "Alex", lastname: "Steiner")
-            })
-        ]
-    ),
-    mutation: GraphQLObjectType(name: "Mutations", fields: [
-        "createUser": GraphQLField(type: userType, args: ["firstname": GraphQLArgument(type: GraphQLString), "lastname": GraphQLArgument(type: GraphQLString)], resolve: { _, arguments, _, _ in
-            User(firstname: arguments["firstname"].string!, lastname: arguments["lastname"].string!)
-        })
-    ])
-)
